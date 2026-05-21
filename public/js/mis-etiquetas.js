88080a79c4871ca0a72578800dd3edaa00eaffbb
Carla Terol Ramírez <hi@ctrlstudio.es>
2026-05-21 10:28:45 +0200

    fix: update navbar and footer links from .html to next.js routes

--- public/js/mis-etiquetas.js
+++ public/js/mis-etiquetas.js
@@ -4,7 +4,7 @@ class MiMenu extends HTMLElement {
         this.innerHTML = `
 <nav class="navbar navbar-expand-lg ctrl-navbar">
   <div class="container-fluid px-0">
-    <a class="navbar-brand ps-3" href="index.html">
+    <a class="navbar-brand ps-3" href="/">
       <img src="img/LOGO.svg" class="logo-svg" alt="CTRL Studio">
     </a>
 
@@ -27,11 +27,11 @@ class MiMenu extends HTMLElement {
       <ul class="navbar-nav ms-auto align-items-lg-center gap-lg-3 px-3 px-lg-0">
 
         <li class="nav-item">
-          <a class="nav-link" href="index.html">INICIO</a>
+          <a class="nav-link" href="/">INICIO</a>
         </li>
 
         <li class="nav-item">
-          <a class="nav-link" href="empresa.html">NOSOTROS</a>
+          <a class="nav-link" href="/empresa">NOSOTROS</a>
         </li>
 
         <li class="nav-item dropdown">
@@ -39,20 +39,20 @@ class MiMenu extends HTMLElement {
             SERVICIOS
           </a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="productos.html">Todos los servicios</a></li>
-            <li><a class="dropdown-item" href="productos.html#branding">Branding</a></li>
-            <li><a class="dropdown-item" href="productos.html#plantillas">Plantillas</a></li>
-            <li><a class="dropdown-item" href="productos.html#planes">Planes</a></li>
+            <li><a class="dropdown-item" href="/productos">Todos los servicios</a></li>
+            <li><a class="dropdown-item" href="/productos#branding">Branding</a></li>
+            <li><a class="dropdown-item" href="/productos#plantillas">Plantillas</a></li>
+            <li><a class="dropdown-item" href="/productos#planes">Planes</a></li>
           </ul>
         </li>
 
         <li class="nav-item">
-          <a class="nav-link" href="contacto.html">CONTACTO</a>
+          <a class="nav-link" href="/contacto">CONTACTO</a>
         </li>
 
         <!-- CARRITO -->
         <li class="nav-item">
-          <a href="carrito.html" class="nav-link d-flex align-items-center gap-1">
+          <a href="/carrito" class="nav-link d-flex align-items-center gap-1">
             <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
               <path d="M6 2L3 6v14a2 2 0 002 2h14a2 2 0 002-2V6l-3-4z"/>
               <line x1="3" y1="6" x2="21" y2="6"/>
@@ -64,7 +64,7 @@ class MiMenu extends HTMLElement {
 
         <!-- USUARIO -->
         <li class="nav-item">
-          <a href="usuario.html" class="btn-carrito" aria-label="Perfil" title="Mi Perfil">
+          <a href="/usuario" class="btn-carrito" aria-label="Perfil" title="Mi Perfil">
           <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
           <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"/><circle cx="12" cy="7" r="4"/>
           </svg>
@@ -116,18 +116,18 @@ class MiPie extends HTMLElement {
       <div class="col-lg-2 col-6">
         <p class="footer-heading">Nosotros</p>
         <ul class="footer-links">
-          <li><a href="empresa.html">Nosotros</a></li>
-          <li><a href="empresa.html">Equipo</a></li>
-          <li><a href="empresa.html">Proceso</a></li>
-          <li><a href="contacto.html">Contacto</a></li>
+          <li><a href="/empresa">Nosotros</a></li>
+          <li><a href="/empresa">Equipo</a></li>
+          <li><a href="/empresa">Proceso</a></li>
+          <li><a href="/contacto">Contacto</a></li>
         </ul>
       </div>
       <div class="col-lg-2 col-6">
         <p class="footer-heading">Productos</p>
         <ul class="footer-links">
-          <li><a href="productos.html#branding">Branding</a></li>
-          <li><a href="productos.html#plantillas">Plantillas</a></li>
-          <li><a href="productos.html#planes">Planes</a></li>
+          <li><a href="/productos#branding">Branding</a></li>
+          <li><a href="/productos#plantillas">Plantillas</a></li>
+          <li><a href="/productos#planes">Planes</a></li>
         </ul>
       </div>
       <div class="col-lg-4">
