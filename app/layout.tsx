8940aaba9c8d6f5341bd3f8282928ae1794a4995
Carla Terol Ramírez <hi@ctrlstudio.es>
2026-05-23 18:07:35 +0200

    estilios

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Script from "next/script";
 import { Providers } from "./providers";
 
 const geistSans = Geist({
@@ -29,14 +28,10 @@ export default function RootLayout({
       lang="es"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <head>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" />
-      </head>
       <body className="min-h-full flex flex-col">
         <Providers>
           {children}
         </Providers>
-        <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" strategy="afterInteractive" />
       </body>
     </html>
   );
